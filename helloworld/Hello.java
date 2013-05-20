29bffeb229db4aa059eda9369690eae3cf678d53
Wolfgang Hofleitner <wolfgang.hofleitner@chello.at>
2013-05-20 18:15:57 +0200

    V01.01.00
    Signed-off-by: Wolfgang Hofleitner <wolfgang.hofleitner@chello.at>

---
+++ helloworld/Hello.java
@@ -0,0 +1,15 @@
+public class Hello
+{
+  public static String world()
+  {
+    return "Hello world";
+<<<<<<< HEAD
+	    return "Hello world";
+  }
+}
+
+return "Hello world";
+=======
+  }
+}
+>>>>>>> 476e2cfcabe5f95a3bbade14ee18e946296ee677
