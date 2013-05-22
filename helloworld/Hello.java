bfa80d18d562ed3f3b41ab991ad55afeb2c54da2
Wolfgang Hofleitner <wolfgang.hofleitner@chello.at>
2013-05-23 00:58:39 +0200

    Cahnges by rtshow 23.05.2013

--- helloworld/Hello.java
+++ helloworld/Hello.java
@@ -3,13 +3,10 @@ public class Hello
   public static String world()
   {
     return "Hello world";
-<<<<<<< HEAD
-	    return "Hello world";
+    return "Hello world";
   }
 }
 
 return "Hello world";
-=======
   }
 }
->>>>>>> 476e2cfcabe5f95a3bbade14ee18e946296ee677
